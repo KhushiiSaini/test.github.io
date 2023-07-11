13b4ce9df0dd1117c658c0bbf86f3a5117eec075
Khushii Saini <89405801+KhushiiSaini@users.noreply.github.com>
2023-07-11 15:40:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-function togglePopup(){
-    document.getElementById("popup-1").classList.toggle("active");
+function openPopup(popupId) {
+    let popup = document.getElementById(popupId);
+    popup.classList.add("open-popup");
 }
+
+function closePopup(popupId) {
+    let popup = document.getElementById(popupId);
+    popup.classList.remove("open-popup");
+}
+
+// Get all close buttons
+let closeButtons = document.querySelectorAll('.popup a.close');
+closeButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        let popup = button.closest('.popup');
+        popup.classList.remove("open-popup");
+    });
+});
